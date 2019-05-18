d6d653eadad8c2a974d914efb997cc2600090dab
xuDouble <1597350404@qq.com>
2019-05-18 22:15:44 +0800

    完成搜索条布局

---
+++ src/components/search.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <!-- 搜索条 -->
+        <div class="search">
+            <div class="search-item">
+                 <icon type='search'/>
+                <span>搜索</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return
+    }
+}
+</script>
+<style scoped>
+.search{
+    padding: 20rpx;
+    background-color: #EB4450;
+}
+.search .search-item{
+text-align: center;
+background-color: #fff;
+}
+.search .search-item icon {
+ vertical-align: middle
+}
+</style>
+
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,11 +1,26 @@
 <template>
-    <div>主页</div>
+
+    <div>
+        <!-- 搜索条 -->
+       <search-bar></search-bar>
+    </div>
 </template>
 
 <script>
+// 导入公共search组件
+import SearchBar from '../../components/search'
 export default {
     data(){
-        return
+        return{
+
+        }
+    },
+    components: {
+        'search-bar':SearchBar
     }
 }
 </script>
+<style scoped>
+
+</style>
+
